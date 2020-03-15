e73a5a8f0287f386eacd6d2ff20046063f25ed9c
살균표백 <NobodyInbody@outlook.com>
2020-03-16 03:11:36 +0900

    기본 파일 정보 표시

--- maxFileTool.py
+++ maxFileTool.py
@@ -9,15 +9,40 @@ RT = pymxs.runtime
 # max파일 프리뷰
 # fbx 익스포트
 print("hellow max python")
+class FileNameSet():
+    ''' ui및 파일 정보를 관리할 데이터셋
+    '''
+    def __init__(self,file_index, path_full, path_dir, file_name, ext, num_head ="", num = "", annotation = ""):
+        self.index = file_index
+        self.full_path = path_full
+        self.dir = path_dir
+        self.name = file_name
+        self.extension = ext
+        self.number_head = num_head
+        self.number_str = num
+        self.annotation = annotation
+    def Set_version_number_up(self):
+        if len(self.number_str) == 0:
+            self.number_str = 0
+        new_number = int(self.number_str)+1
+        if len(new_number) < 2:
+            new_number = '0'+ str(new_number)
+        self.number_str = str(new_number) 
+        self.full_path = self.dir +  self.name + ", V" + self.number_head + self.number_str + '_' + self.annotation + self.extension 
+    def Get_version_str(self):
+        return ("V" + self.number_head + self.number_str)
+
+
 
 class FileToolUI(QtWidgets.QDialog):
-
+    alphabet_lower_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     def __init__(self, parent=MaxPlus.GetQMaxMainWindow()):
         super(FileToolUI, self).__init__(parent)
         RT.clearlistener()
-        self.main_dir_path = RT.getFilenamePath(RT.maxfilepath)
-        self.current_MaxFilePath = RT.getFilenamePath(RT.maxfilepath)
-        self.current_maxfile_name = RT.maxFileName
+        self.m_main_dir_path = RT.getFilenamePath(RT.maxfilepath)
+        self.m_current_MaxFilePath = RT.getFilenamePath(RT.maxfilepath)
+        self.m_current_maxfile_name = RT.getFilenameFile(RT.maxFileName).split(',')[0]
+        self.fileSet_List = []
         self.setWindowTitle(u"파일툴")
         self.initUI()
 
@@ -27,55 +52,106 @@ class FileToolUI(QtWidgets.QDialog):
         self.maxfile_list_layout = QtWidgets.QVBoxLayout()
         self.inputFileNameLayout = QtWidgets.QHBoxLayout()
         self.button_layout = QtWidgets.QHBoxLayout()
-        
         self.main_layout.addLayout(self.maxfile_list_layout)
         self.main_layout.addLayout(self.inputFileNameLayout)
         self.main_layout.addLayout(self.button_layout)
         # ui객체
-        self.dirLabel = QtWidgets.QLabel(u"[대상 경로] " + self.main_dir_path)
+        self.dirLabel = QtWidgets.QLabel(u"[대상 경로] " + self.m_main_dir_path)
         self.filesList_tree_widget = QtWidgets.QTreeWidget()
         self.filesList_tree_widget.setHeaderLabel(u"파일이름")
-        self.maxFileNameEdit = QtWidgets.QLineEdit(self.current_maxfile_name, self)
-        self.fileAnnotaionEdit = QtWidgets.QLineEdit(u"주석", self)
+        # 
+        self.maxFileNameEdit = QtWidgets.QLineEdit(self.m_current_maxfile_name, self)
+        self.version_label = QtWidgets.QLabel(u"Va00")
+        self.fileannotationEdit = QtWidgets.QLineEdit(u"주석", self)
+        # 버튼 생성
         self.saveMaxBtn = QtWidgets.QPushButton(u"max로 저장")
+        self.savePlueMaxBtn = QtWidgets.QPushButton(u"버전업 저장")
         self.saveFbxBtn = QtWidgets.QPushButton(u"fbx로 저장")
         self.openFolder = QtWidgets.QPushButton(u"경로 열기")
-
-        for i in range(1,6):
-            item = QtWidgets.QTreeWidgetItem(self.filesList_tree_widget)
-            item.setText(0, str(i))
-        
+        # 파일리스트 위젯추가
         self.maxfile_list_layout.addWidget(self.dirLabel)
         self.maxfile_list_layout.addWidget(self.filesList_tree_widget)
+        # 파일입력 위젯추가
         self.inputFileNameLayout.addWidget(self.maxFileNameEdit)
-        self.inputFileNameLayout.addWidget(self.fileAnnotaionEdit)
-        
-        # 레이아웃에 버튼 추가
+        self.inputFileNameLayout.addWidget(self.version_label)
+        self.inputFileNameLayout.addWidget(self.fileannotationEdit)
+        # 버튼 위젯 추가
         self.button_layout.addWidget(self.saveMaxBtn)
+        self.button_layout.addWidget(self.savePlueMaxBtn)
         self.button_layout.addWidget(self.saveFbxBtn)
         self.button_layout.addWidget(self.openFolder)
-
-        self.openFolder.clicked.connect(self.OpenCurrentMaxFile)
-
+        # 연결
+        self.openFolder.clicked.connect(self.OpenCurrentMaxFile) #clicked.connects는 인자명으로 전달, 함수 아님
+        # 메인 레아아웃 추가
         self.setLayout(self.main_layout)
         self.updateUI()
-        
+    def printFileNameSet(self, name_set):
+        pass
     def updateUI(self):
         print(u"updateUI")
+        self.GetFileList()
+        # 열려있는 파일 정보 업데이트
+        current_file_set = self.GetCurrentFileNameSet()
+        self.version_label.setText(current_file_set.Get_version_str())
+        self.fileannotationEdit.setText(current_file_set.annotation)
+    def GetNewFileNameSet(self,file_index, file_full_path_str):
+        ''' FileNameSet 클래스 반환 '''
+        file_path = RT.getFilenamePath(file_full_path_str)
+        file_fullName = RT.filenameFromPath(file_full_path_str)
+        temp_list = RT.getFilenameFile(file_fullName).split(',')
+        file_name = temp_list[0]
+        file_ext = RT.getFilenameType(file_fullName)
+        number_head = ""
+        number = ""
+        annotation = ""
+        is_Version_file = False
+        if len(temp_list) > 1 :
+            split1_list = temp_list[1].split('_') # ㅁㅁ, Va00
+            test_str_list = split1_list[0].split()
+            if test_str_list[0][0] == "V" and len(test_str_list[0]) == 4:
+                if test_str_list[0][1] in FileToolUI.alphabet_lower_list:
+                    is_Version_file = True
+            if is_Version_file:
+                number_head = test_str_list[0][1]
+                number = test_str_list[0][2:4]
+                print('버전수:'+ str(number))
+            else:
+                annotation = temp_list[1]
+            if is_Version_file and len(split1_list) > 1:
+                annotation = split1_list[1]
+        return FileNameSet(file_index, file_full_path_str, file_path, file_name, file_ext, number_head, number, annotation)
+        
+    def GetCurrentFileNameSet(self):
+        path_full = RT.maxfilepath + RT.maxfileName
+        current_file_nameSet = self.GetNewFileNameSet(-1, path_full)
+        return current_file_nameSet
+    
+    def GetFileList(self, maxfiles = []):
+        maxfiles = RT.GetFiles(self.m_current_MaxFilePath + "*.max")
+        self.fileSet_List = []
+        # 파일을 수집하여 네임셋으로 저장
+        file_index = 0
+        for file_full_path_str in maxfiles:
+            new_file_set = self.GetNewFileNameSet(file_index, file_full_path_str)
+            self.fileSet_List.append(new_file_set)
+            file_index = file_index + 1
+        max_index = 0
+        for file_str in maxfiles:
+            file_name = RT.filenameFromPath(file_str)
+            item = QtWidgets.QTreeWidgetItem(self.filesList_tree_widget)
+            item.setText(0, self.fileSet_List[max_index].name)
+            max_index = max_index + 1
 
-    def GetFileList(self):
+    def SaveMaxFile(self, fileName, annotation):
+        self.filesList_tree_widget.selectedItems()
+    def SaveVersionUp(self):
         pass
-
-    def SaveMaxFile(self, fileName, annotaion):
-        pass
-
     def ExportFBX(self, fileName):
         pass
-
     def InPutFileName(self, fileNameString):
         pass
     def OpenCurrentMaxFile(self):
-        RT.ShellLaunch(self.current_MaxFilePath, "")
+        RT.ShellLaunch(self.m_current_MaxFilePath, "")
 
 #맥스 스크립트 창에서는 사용 못함 즉 필요없음. 
 #if __name__ == "__main__":
