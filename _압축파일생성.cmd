ca5b8f51c812d0a425b35cac2d14d256058dddcc
nobodyInbody <nobodyInbody@outlook.com>
2021-01-26 03:13:30 +0900

    압축옵션수정했음

--- _압축파일생성.cmd
+++ _압축파일생성.cmd
@@ -2,7 +2,7 @@
 chcp 65001
 SET ARCHIVE="C:\Program Files\7-Zip\7z.exe"
 SET filename=Max파일관리툴
-SET PARAMETERS=a
+SET PARAMETERS=a -tzip
 
 REM Korean OS = YYYY-MM-DD 
 FOR /F "tokens=1-4 delims=- " %%i IN ('date /t') DO SET MZP_FILE=%filename%_%%i-%%j-%%k.mzp
